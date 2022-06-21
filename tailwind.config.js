4fd09428e68b269c9422ccee7fb731ad348682a4
Elian-beep <elianoliver101@gmail.com>
2022-06-21 12:16:45 -0300

    all: setup do tailwind e conexão com graphQL

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx' //Todos os meus arquivos tsx terão a config tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
